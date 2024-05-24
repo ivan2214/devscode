4da76bfa970ae32abfe1b2ebc5fbc483b0bdf068
Ivan Bongiovanni <bongiovanniivan12gmail.com>
2024-05-24 16:31:39 -0300

    delete migrations

--- prisma/migrations/20240524190806_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `replyId` on the `Reply` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Reply" DROP COLUMN "replyId";
